db61a8c8fb44a8286f6eb7e6e042092d3443dcb3
Lujain-06 <lujain.o.b.27@gmail.com>
2025-05-17 08:21:36 +0000

    add code to find lat and long

--- DistancesBitweenCities.py
+++ DistancesBitweenCities.py
@@ -4,7 +4,6 @@ from geopy.geocoders import Nominatim
 
 os.listdir() # List files in current directory 
 
-
 def load_csv(file_name): # mitigate the risk of FileNotFoundError
     if not os.path.exists(file_name):
         raise FileNotFoundError(f"Data file '{file_name}' missing")
@@ -22,6 +21,7 @@ def get_geocode(city): # find the latitude and longitude from the city name
         return location.latitude, location.longitude
     else:
         print(location," not found.")
+        
 def get_coordinates(agent_city,order_city): # get the latitude and longitude
     agent_lat, agent_lng = get_geocode(agent_city)
     order_lat, order_lng = get_geocode(order_city)
