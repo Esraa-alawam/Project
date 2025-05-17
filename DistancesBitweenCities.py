dd1000597b1fb0ff4630f4c09a09395f4849c4ca
Esraa <esraa@ubuntu22.myguest.virtualbox.org>
2025-05-17 23:04:48 +0300

    Replace old code with new coordinates script

--- DistancesBitweenCities.py
+++ DistancesBitweenCities.py
@@ -1,34 +1,27 @@
-import pandas as pd
-import os 
 from geopy.geocoders import Nominatim
+import sys
 
-os.listdir() # List files in current directory 
-
-
-def load_csv(file_name): # mitigate the risk of FileNotFoundError
-    if not os.path.exists(file_name):
-        raise FileNotFoundError(f"Data file '{file_name}' missing")
-    return pd.read_csv(file_name)
-
-dfAgent = load_csv("expanded_agents_data.csv") # Agent location
-dforder = load_csv("expanded_orders_data.csv") # Order destination
-
-# Initialize geocoder
-nom = Nominatim(user_agent="find the latitude and longitude")
-
-def get_geocode(city): # find the latitude and longitude from the city name
+def get_geocode(city):
+    nom = Nominatim(user_agent="geoapiExercises")
     location = nom.geocode(city)
-    if location: # if the location is found
+    if location:
         return location.latitude, location.longitude
     else:
         return None, None
-        
-def get_coordinates(agent_city,order_city): # get the latitude and longitude
+
+if _name_ == "_main_":
+    if len(sys.argv) != 3:
+        print("not found")
+        sys.exit()
+
+    agent_city = sys.argv[1]
+    order_city = sys.argv[2]
+
     agent_lat, agent_lng = get_geocode(agent_city)
     order_lat, order_lng = get_geocode(order_city)
-    
-    if None in (agent_lat, agent_lng, order_city):
-      return "not found"
-      
-    return f"{agent_lat},{agent_lng};{order_lat},{order_lng})"
+
+    if None in (agent_lat, agent_lng, order_lat, order_lng):
+        print("not found")
+    else:
+        print(f"{agent_lat},{agent_lng};{order_lat},{order_lng}")
 
